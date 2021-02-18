0e59f8054b5a42e9bd59f231c72c947c237aee2b
nitish <nitish.singh158@gmail.com>
2021-02-18 13:34:12 -0700

    aap.py file added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 import pymongo
 import pickle
 from sklearn.preprocessing import StandardScaler
@@ -9,9 +9,8 @@ import numpy as np
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'WokeRUS'
 app.config['MONGO_URI'] = "mongodb://localhost:27017/WokeRUS"
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
-glo_cs = ''
 
 # Classification model
 model_classfy = pickle.load(open('model_calssify.pkl', 'rb'))
@@ -58,49 +57,32 @@ def interest():
 
 @app.route('/interest', methods=['POST'])
 def calculation():
-       # cs = np.float64(request.form['Credit_Score'])
-       # print (type(cs))
-       # eml = np.float64(request.form['Emp_Length'])
-       # dti = np.float64(request.form['Debt_income_ratio'])
-       # loa = np.float64(request.form['Loan_Amount'])
-       # ai = np.float64(request.form['annual_inc'])
-       # inq = np.float64(request.form['inq_last_6mths'])
-       # acc = np.float64(request.form['mo_sin_rcnt_tl'])  
-       cs = request.form['Credit_Score']
-       # print (type(cs))
-       eml = request.form['Emp_Length']
-       dti = request.form['Debt_income_ratio']
+ 
        loa = request.form['Loan_Amount']
+       dti = request.form['Debt_income_ratio']
+       eml = request.form['Emp_Length']
+       cs = request.form['Credit_Score']
        ai = request.form['annual_inc']
        inq = request.form['inq_last_6mths']
        acc = request.form['mo_sin_rcnt_tl']   
-       # if float(cs) < 600 or float(dti) > 45:
-       #        return render_template('interest.html', prediction_text = 'your application is rejected')
-       # else:
-       # features = [float() for x in request.form.values()]
-       # print(request.form.values)
-       # final_features = np.array(features)
-       features = [int(x) for x in request.form.values()]
-       final_features = [np.array(features)]
-       # final_feature1 = final_features.reshape(1,-1)
-       # scaler = MinMaxScaler()
-       scaled = model_scaler_regression.fit_transform(final_features)
 
-       # features = np.array([cs,eml,dti,loa, ai,inq,acc])
-       # features = [np.array([cs, eml, dti, loa, ai, inq, acc])]
-       # features = np.array([cs, eml, dti, loa, ai, inq, acc])
-       # feature1 = features.reshape(1,-1)
+       features = [float(x) for x in request.form.values()]
+       final_features = [np.array(features)]
+  
+       scaler = StandardScaler()
+       scaled = scaler.fit_transform(final_features)
+       
        print(features)
        print(final_features)
-       # print(final_feature1)
        print(scaled)
-       # print(feature1)
-              # scaler = StandardScaler()
-              # scaled_inputs = scaler.fit_transform (features1)
        predictions = model_regression.predict(scaled)
        print (predictions)
-       output = predictions[0]
-       return render_template('interest.html', prediction_text = output)
+       output = round(predictions[0],0)
+       print(output)
+       predictions_low = output-3
+       predictions_high = output+3
+       prediction_range = f'Your estimated interesed is between {predictions_low}% - {predictions_high}%'
+       return render_template('interest.html', prediction_text = prediction_range)
 
 
 
